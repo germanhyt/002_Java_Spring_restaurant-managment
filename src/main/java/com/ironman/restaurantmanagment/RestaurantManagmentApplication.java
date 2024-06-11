ee592b51759b0d9de56387f22a00e7f9c7884401
Dacounter95 <146789960+germanhytsystem@users.noreply.github.com>
2024-06-11 17:31:21 -0500

    feat: Start Project

---
+++ src/main/java/com/ironman/restaurantmanagment/RestaurantManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.ironman.restaurantmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironman/restaurantmanagment/RestaurantManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironman.restaurantmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
